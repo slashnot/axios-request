5eb50494d473ff05abca1f94553749adfe47e8f6
Ramkumar <slashnot84@gmail.com>
2018-11-30 17:31:05 +0530

    Dynamic config setting

--- public/myapp.js
+++ public/myapp.js
@@ -1,5 +1,6 @@
 
 var post = document.querySelector('ax-post');
+var ax = document.querySelector('ax-config');
 
 // post.post("http://localhost:3000/users", {name: "New Name"}, function(res){
 //     console.log(res)
